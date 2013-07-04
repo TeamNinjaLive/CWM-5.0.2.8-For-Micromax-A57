5a518790b873677c38fb767d4aca070c3bee57f3
Dhinesh Kumar <dhinesh77@ymail.com>
2013-07-04 06:41:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+CWM Based Recovery v5.0.2.8
+
+Ported for Micromax A57
+
+Thanks to koush for cwm source and guides
---
+++ boot.img-ramdisk/sbin/qrdsys.sh
@@ -0,0 +1,6 @@
+#!/sbin/sh
+#rmmod ar6000
+echo 100 > /sys/class/leds/lcd-backlight/brightness
+echo "Macle test"
+exit 0
+
